22e747328e54be2b13c139cd1fc83375e95d421a
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-07-05 17:15:43 -0400

    Add WebStorm installation support

--- src/applications/webstorm/index.js
+++ src/applications/webstorm/index.js
@@ -3,3 +3,11 @@
 export function settings(){
 
 }
+
+export function install() {
+	if (config.isOsx) {
+		return shell.run('brew cask install webstorm');
+	} else if (config.isWindows) {
+		return shell.run('choco install webstorm');
+	}
+}
