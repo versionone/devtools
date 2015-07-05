e770d4bdad1c639413f57a0d67f20ae94f3b929a
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-07-05 17:06:29 -0400

    Added installation of Atom

--- src/applications/atom/index.js
+++ src/applications/atom/index.js
@@ -1,9 +1,17 @@
 'use strict';
 
-import {mklink} from './../../utils';
+import {shell, mklink} from './../../utils';
 import config from './../../config';
 import path from 'path';
 
 export function settings(){
 	return mklink(path.join(__dirname, './config.cson'), path.join(config.home, '.atom/config.cson'));
+}
+
+export function install() {
+	if (config.isOsx) {
+		return shell.run('brew cask install atom');
+	} else if (config.isWindows) {
+		return shell.run('choco install Atom');
+	}
 }
