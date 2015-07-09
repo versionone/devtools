05e4d94a3351b4b3181bac98e548bbb0dfa92055
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-07-09 09:40:31 -0400

    Create settings directory for Visual Studio before trying to link settings files to that directory

--- src/applications/visualStudio/index.js
+++ src/applications/visualStudio/index.js
@@ -3,32 +3,42 @@
 import config from './../../config';
 import {readdir, mklink} from './../../utils';
 import path from 'path';
+import mkdirp from 'mkdirp';
 
 export function settings() {
-	if (!config.isWindows) {
-		return new Promise(resolve=> {
-			resolve(true)
-		});
-	}
-	readdir(path.join(__dirname, './'))
-		.then(function (fileItems) {
-			fileItems
-				.filter(function (item) {
-					return fs.lstatSync(item).isFile();
-				})
-				.filter(function (item) {
-					var visualStudioSettings = /\.vssettings$/;
-					return visualStudioSettings.exec(item);
-				})
-				.forEach(function (item) {
-					mklink(path.join(__dirname, item), path.join(config.home, 'Documents/Visual Studio 2013/Settings', item));
+	return new Promise((resolve, reject)=> {
+		if (!config.isWindows) {
+			resolve(true);
+			return;
+		}
+		var settingsDir = path.join(config.home, 'Documents/Visual Studio 2013/Settings');
+		mkdirp(settingsDir, error=> {
+			if (error) {
+				reject(error);
+				return;
+			}
+			readdir(path.join(__dirname, './'))
+				.then(function (fileItems) {
+					var promises = fileItems
+						.filter(function (item) {
+							return fs.lstatSync(item).isFile();
+						})
+						.filter(function (item) {
+							var visualStudioSettings = /\.vssettings$/;
+							return visualStudioSettings.exec(item);
+						})
+						.map(function (item) {
+							return mklink(path.join(__dirname, item), settingsDir, item);
+						});
+					resolve(promises);
 				});
 		});
+	});
 }
 
 export function install() {
 	console.log('installing visual studio...');
 	return new Promise(resolve=> {
-		resolve(true) ;
+		resolve(true);
 	});
 }
