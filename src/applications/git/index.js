b85c59d6a1531667c5bdd98d8bcad996bd4fe342
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-09-04 13:20:52 -0400

    Updating diffmerge to be a custom shell script for Windows and OSX; includes titles when merging

--- src/applications/git/index.js
+++ src/applications/git/index.js
@@ -1,28 +1,30 @@
 'use strict';
 
-import {readdir, mklink, shell} from './../../utils';
+import {
+	readdir, mklink, shell
+}
+	from './../../utils';
 import path from 'path';
 import config from './../../config';
 import fs from 'fs';
 
 export function settings() {
 	return readdir(path.join(__dirname, './'))
-		.then(files=> {
+		.then(files => {
 			files
-				.filter(item=> {
-					return /\.global$/.exec(item);
-				})
-				.forEach(item=> {
-					let newLink = item.replace('.global', '');
-					mklink(path.join(__dirname, item), path.join(config.home, newLink));
-				});
-			files
-				.filter(item=> {
-					return item === 'git-completion.bash' || item === 'git-prompt.sh';
-				})
-				.forEach(item=> {
+				.map(item => item.replace(/\.global$/))
+				.filter(/diffmerge/.exec)
+				.forEach(item => {
 					mklink(path.join(__dirname, item), path.join(config.home, item));
 				});
+			let diffmerge = files.find(/diffmerge/.exec);
+			let gitMergeToolsDir;
+			if (config.isOsx) {
+				gitMergeToolsDir = '/usr/local/git/libexec/git-core/mergetools';
+			} else if (config.isWindows) {
+				gitMergeToolsDir = 'c:\\Program Files (x86)\\Git\\libexec\\git-core\\mergetools';
+			}
+			mklink(path.join(__dirname, diffmerge), path.join(gitMergeToolsDir, diffmerge));
 		});
 }
 
@@ -33,7 +35,7 @@ export function install() {
 	} else if (config.isWindows) {
 		return shell('choco install git -y');
 	}
-	return new Promise(resolve=> {
+	return new Promise(resolve => {
 		resolve(true);
 	});
 }
